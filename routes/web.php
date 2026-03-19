f38e0beef4f3477b74edc949c00cd5fe928468d8
ciroviscardiAlgomera <ciro.viscardi@algomera.it>
2026-03-19 11:47:09 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/import', function () {
+    echo "Import page";
+     return view('import');
+});
